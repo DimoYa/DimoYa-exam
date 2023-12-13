ba6076b1eebafabf5de8552329c2725a9ce8ebc0
DimoYa <dimo_yanev@abv.bg>
2023-12-13 20:09:59 +0200

    add UI tests to Ci/CD

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add products page', async ({ page }) => {
-    await page.goto('http://localhost:8090/Add-Product');
+    await page.goto('https://dimoya-exam.onrender.com/Add-Product');
     const form = await page.$('input');
     expect(form).toBeTruthy();
   });
